6957ba66968ef1b91c22f87c9b2719dc03e5678b
Nitin Chandani <nitin1chandani@gmail.com>
2023-10-28 11:28:14 +0530

    final commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const bodyParser = require("body-parser");
-const port = 4000;
+const port = 3000;
 app.use(bodyParser.json());
 
 let todos = [];
